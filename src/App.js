1e9c20396a52542da4ac0b9fe44c886692b658ff
Dean Simonds <alandeansimonds@gmail.com>
2019-04-09 18:50:39 +1000

    Making person with dynamic content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>I am a react app</h1>
+        <h1>I am a React App</h1>
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
     );
   }
