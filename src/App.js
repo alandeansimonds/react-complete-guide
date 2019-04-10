5331e7e0a43161732ad1eda2f741621a723c7745
Dean Simonds <alandeansimonds@gmail.com>
2019-04-10 10:55:38 +1000

    Changes added with button state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    //DON'T DO THIS: this.state.person[0].name = 'Maximilian';
+    this.setState({persons: [
+      { name: 'Maximilian', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 27 }
+    ]
+  })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>I am a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
